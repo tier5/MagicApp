562e4564d7c97dbabf47a78fccaeaee90bdf7b9f
Tier5 LLC <github@tier5.us>
2018-01-02 18:55:51 +0530

    initial setup for laravel & node

--- models/index.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/magicAppDb').then((db)=>{
-    console.log('Connected to db')
-  }).catch((err)=>{
-    console.log(err);
-  })
- module.exports= mongoose
