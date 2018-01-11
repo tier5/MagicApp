2f95e996c880331d05bbf56b674611a82834373b
tier5 <work@tier5.us>
2018-01-11 20:45:51 +0530

    Save data from script v1.1

--- app/Http/Controllers/ZapierController.php
+++ app/Http/Controllers/ZapierController.php
@@ -83,20 +83,65 @@ class ZapierController extends Controller
         }
     }
 
+    /**
+     * @param Request $request
+     * @return JSON
+     * Validating params with database params field
+     * Storing data from the script into database.
+     */
     public function saveScriptData(Request $request)
     {
-        \Log::info(print_r($request->all(),true));
-        $zapId  = $request['zapId'];
-        $params = $request['params'];       //array containing params
+        //Log::info(print_r($request->all(),true));
+
+        $zapId  = $request['zap']['zapId'];
+
+        $params = $request['zap']['params'];       //array containing params
         if( $zapId != "" ){
-
-            $getAllZapParams = Zapfields::where('zap_id',$zapId)->select('zap_field','validation_id','zap_value')->get();
-
+            $validationCount = 0;
+            $zapFieldArray=[];
+            $getAllZapParams = Zapfields::where('zap_id',$zapId)->select('id','zap_field','validation_id','zap_value')->get();
             if( count($getAllZapParams) != 0 ){
-
                 foreach($getAllZapParams as $key=>$zapparams){
                     foreach($params as $paramsKey=>$paramsValue){
-                         // do Something with the data
+                        // do Something with the data
+                        if ($zapparams->zap_field ==$paramsKey){
+                            $zapParam= Zapfields::where('zap_id',$zapId)->where('zap_field',"=",$paramsKey)->select('zap_value','validation_id')->first();
+                            if ($zapParam->validation_id == 1){
+                                if (count($zapParam)!=0){
+                                    $validationCount +=1;
+                                    $zapFieldArray[$validationCount]['zap_id'] = $zapId;
+                                    $zapFieldArray[$validationCount]['zap_field_id']= $zapparams['id'];
+                                    $zapFieldArray[$validationCount]['zap_value']= $paramsValue;
+                                    $zapFieldArray[$validationCount]['url']= $request['zap']['location'];
+                                }
+                            }else if($zapParam->validation_id == 2){
+                                if ($zapParam->zap_value == $paramsValue){
+                                    $validationCount +=1;
+                                    $zapFieldArray[$validationCount]['zap_id'] = $zapId;
+                                    $zapFieldArray[$validationCount]['zap_field_id']= $zapparams['id'];
+                                    $zapFieldArray[$validationCount]['zap_value']= $paramsValue;
+                                    $zapFieldArray[$validationCount]['url']= $request['zap']['location'];
+                                }
+                            }else if($zapParam->validation_id == 3){
+                                if ($zapParam->zap_value != $paramsValue){
+                                    $validationCount +=1;
+                                    $zapFieldArray[$validationCount]['zap_id'] = $zapId;
+                                    $zapFieldArray[$validationCount]['zap_field_id']= $zapparams['id'];
+                                    $zapFieldArray[$validationCount]['zap_value']= $paramsValue;
+                                    $zapFieldArray[$validationCount]['url']= $request['zap']['location'];
+                                }
+                            }
+                        }
+                    }
+                }
+                if ($validationCount == 3){
+                    foreach ($zapFieldArray as $key=>$zapValue){
+                        $saveZapData = new Zapdata;
+                        $saveZapData->zap_id = $zapValue['zap_id'];
+                        $saveZapData->zap_field_id = $zapValue['zap_field_id'];
+                        $saveZapData->zap_value =  $zapValue['zap_value'];
+                        $saveZapData->url =  $zapValue['url'];
+                        $saveZapData->save();
                     }
                 }
             }else{
@@ -107,7 +152,6 @@ class ZapierController extends Controller
                 ),403);
             }
         }else{
-
             return Response::json(array(
                 'status'   => false,
                 'response' => [],
