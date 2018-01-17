00f12917ef851e9f34d869ecaf4070e6a0399037
Tier5 LLC <github@tier5.us>
2018-01-17 18:11:17 +0530

    v1.1

--- public/frontend/src/main.js
+++ public/frontend/src/main.js
@@ -6,8 +6,7 @@ import store from './store/index';
 import Vuelidate from 'vuelidate'
 
 Vue.use(VueResource);
-Vue.use(Vuelidate)
-//var node_env = process.env.NODE_ENV;
+Vue.use(Vuelidate);
 import config from '../config/config';
 if(process.env.NODE_ENV =='development'){
   Vue.http.options.root = config.dev_url;
