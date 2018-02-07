a4752099182d4dd590d881c59f148f1c9ea10f54
Tier5 LLc <github@tier5.us>
2018-02-07 22:51:58 +0530

    updated the indentation

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,30 +18,35 @@ function userRegister(req,res,next){
   body.planId = body.plan.id;
   var userType = body.userType; // 'paid' or 'free'
   createCustomer(body,userType)
-      .then((customer)=>{
-        body.customerId = customer.id;
-        return createSubscription(body.customerId,body.planId)
-      })
-      .then((subscription)=>{
-        body.subscriptionId = subscription.id;
-        var user = new Users(body)
-        return user.save();
-      })
-      .then((user)=>{
-        var sendUserData = {
-          email : user.email,
-          name : user.name,
-          isAdmin: user.isAdmin,
-          isActive:user.isActive
-        }
-        res.status(200).send({status:true,message:"User created", token:user.accessToken , user:sendUserData})
-      })
-      .catch((err)=>{
-        console.log(err);
-        deleteCustomer(body.customerId).then(confirmed => console.log('deleted')).catch(err=>console.log(err));
-        return res.status(400).send({status:false,message: 'Somthing Went Wrong!'});
-      })
+        .then((customer)=>{
+            body.customerId = customer.id;
+            return createSubscription(body.customerId,body.planId)
+        })
+        .then((subscription)=>{
+            body.subscriptionId = subscription.id;
+            var user = new Users(body)
+            return user.save();
+        })
+        .then((user)=>{
+            var sendUserData = {
+                email : user.email,
+                name : user.name,
+                isAdmin: user.isAdmin,
+                isActive:user.isActive
+            }
+            return res.status(200).send({status:true,message:"User created", token:user.accessToken , user:sendUserData})
+        })
+        .catch((err)=>{
+            console.log(err);
+
+            deleteCustomer(body.customerId)
+                .then(confirmed => console.log('deleted'))
+                .catch(err=>console.log(err));
+
+            return res.status(400).send({status:false,message: 'Somthing Went Wrong!'});
+        })
 }
+
 /**
  * function to login a user
  * @param {object} req 
@@ -52,27 +57,32 @@ function userRegister(req,res,next){
 function userLogin(req,res,next){
     var { email, password } = req.body;
 
-    Users.findOne({email}).then(user=>{
-        bcrypt.compare(password, user.password, (err, data) => {
-            if (data) {
-                var sendUserData ={
-                    email : user.email,
-                    name : user.name,
-                    isAdmin: user.isAdmin,
-                    isActive:user.isActive,
-                }
-                return res.status(200).send({status:true,message:"success", token:user.accessToken , user:sendUserData})
-                
-            } else {
-                console.log(err);
-                return res.status(400).send({status:false, err: err, message: 'Password Incorrect !'});
-            }
-        });
+    Users
+        .findOne({email})
+        .then(user=>{
+            bcrypt
+                .compare(password, user.password, (err, data) => {
+                    if (data) {
+                        var sendUserData ={
+                            email : user.email,
+                            name : user.name,
+                            isAdmin: user.isAdmin,
+                            isActive:user.isActive,
+                        }
+                        return res.status(200).send({status:true,message:"success", token:user.accessToken , user:sendUserData})
+                        
+                    } else {
+                        console.log(err);
+                        return res.status(400).send({status:false, err: err, message: 'Password Incorrect !'});
+                    }
+                });
 
-    }).catch((err)=>{
-        return res.status(400).send({status:false, message:'User not Exists!' });
-    })
+        })
+        .catch((err)=>{
+            return res.status(400).send({status:false, message:'User not Exists!' });
+        })
 }
+
 /**
  * function to get users list
  * @param {object} req 
@@ -117,6 +127,7 @@ function getAllUsers(req,res,next){
     })
   
 }
+
 /**
  * function to update user
  * @param {object} req 
@@ -130,26 +141,30 @@ function updateUser(req,res,next){
     var isActive = req.body.isActive;
     var user_id = req.params.id;
 
-    isUserAdmin(token).then(()=>{   
-        var conditions = { _id:user_id }
-        ,   update = { $set: { isActive : isActive}}
-        ,   options = { multi: false };
+    isUserAdmin(token)
+        .then(()=>{   
+            var conditions = { _id:user_id }
+            ,   update = { $set: { isActive : isActive}}
+            ,   options = { multi: false };
 
-        Users.update(conditions,update,options).then((data)=>{
+            Users
+                .update(conditions,update,options)
+                .then((data)=>{
 
-            if(data.ok ==1 && data.n==1 && data.nModified==1){
-                return res.status(200).send({message:'Updated',status:true})
-            }
+                    if(data.ok ==1 && data.n==1 && data.nModified==1){
+                        return res.status(200).send({message:'Updated',status:true})
+                    }
+
+                }).catch(function(err){
+                    console.log(err)
+                    return res.status(400).send({message:'Something went wrong',status:false})
+                })
 
         }).catch(function(err){
-
-            console.log(err)
-        })
-
-    }).catch(function(err){
-        return res.status(403).send({message:'Forbidden',status:false})
-    }) 
+            return res.status(403).send({message:'Forbidden',status:false})
+        }) 
 }
+
 /**
  * function to check token is for admin or not 
  * @param {string} token
