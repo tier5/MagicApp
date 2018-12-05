ae1608b4161bccc0cd59277aae2f0937d42f2fb8
Tier5 <work@tier5.us>
2018-12-05 14:07:51 +0530

    baseline

---
+++ src/views/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <h1>This is about component</h1>
+    <Demo/>
+  </div>
+</template>
+
+<script>
+import Demo from '../components/Demo.vue';
+
+export default {
+    components: {
+      Demo
+    }
+  }
+</script>
+
+
+<style>
+
+</style>
