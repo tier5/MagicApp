c80df76026dea987d663b0c521e31e7d0cd71c19
tier5Development <work@tier5.us>
2018-12-29 16:21:03 +0530

    socket implementation backend v0.2

---
+++ helpers/socket.js
@@ -0,0 +1,42 @@
+/**
+ * Helper function to implement socket IO
+ * 
+ */
+
+ var IO_GLOBAL = null;
+ /**
+  * Initiate The Socket
+  * @param {object} IO 
+  */
+ function socketHelperFunction(IO) {
+    IO_GLOBAL = IO;
+    IO.on('connection', (socket)=>{
+        //console.log('New user connected' , socket.id);
+    
+        socket.on('disconnect', ()=> {
+            //console.log(socket.id);
+        })
+
+        socket.on('join', (data)=>{
+            if (data && data.user){
+                //console.log('in join ', data);
+                socket.join(data.user);
+            }
+        })
+     });
+ }
+
+ /**
+  * This function send message to the client to refresh the 
+  * @param {object} user 
+  */
+ function sendRefreshStats(user){ 
+    var userEmail = user.email;
+    IO_GLOBAL.sockets.in(userEmail).emit('refresh-stats', {});
+ }
+
+ module.exports = {
+    socketHelperFunction,
+    sendRefreshStats
+ }
+
